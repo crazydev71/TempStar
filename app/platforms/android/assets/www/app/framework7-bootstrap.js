7213939a98ac804b4e3d1b8d40575d2601a09de7
Mike Betts <mike@freshsqueezedwebsites.com>
2016-08-17 10:43:17 -0400

    app ui prototype

--- app/platforms/android/assets/www/app/framework7-bootstrap.js
+++ app/platforms/android/assets/www/app/framework7-bootstrap.js
@@ -1,16 +1,18 @@
 
 (function () {
-if (Framework7.prototype.device.android) {
+if ( false ) {
+//if (Framework7.prototype.device.android) {
       Dom7('head').append(
-          '<link rel="stylesheet" href="f7/css/framework7.material.min.css">' +
-          '<link rel="stylesheet" href="f7/css/framework7.material.colors.min.css">' +
-          '<link rel="stylesheet" href="app/app.css">'
+          '<link rel="stylesheet" href="lib/framework7/css/framework7.material.min.css">' +
+          '<link rel="stylesheet" href="lib/framework7/css/framework7.material.colors.min.css">' +
+          '<link rel="stylesheet" href="app/app.css">' +
+          '<link rel="stylesheet" href="app/android.css">'
       );
   }
   else {
       Dom7('head').append(
-          '<link rel="stylesheet" href="f7/css/framework7.ios.min.css">' +
-          '<link rel="stylesheet" href="f7/css/framework7.ios.colors.min.css">' +
+          '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.min.css">' +
+          '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.colors.min.css">' +
           '<link rel="stylesheet" href="app/app.css">'
       );
   }
--- app/www/app/app.js
+++ app/www/app/app.js
@@ -2,6 +2,8 @@
 // Determine theme depending on device
 var isAndroid = Framework7.prototype.device.android === true;
 var isIos = Framework7.prototype.device.ios === true;
+var userLoggedIn = false;
+var isDentist = true;
 
 // Set Template7 global devices flags
 Template7.global = {
@@ -12,7 +14,7 @@ Template7.global = {
 // Define Dom7
 var $$ = Dom7;
 
-// Change Through navbar layout to Fixed
+// If on Android, change Through navbar layout to Fixed
 if (isAndroid) {
     // Change class
     $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
@@ -20,65 +22,358 @@ if (isAndroid) {
     $$('.view .navbar').prependTo('.view .page');
 }
 
-// Init App
-var myApp = new Framework7({
+// Setup App
+var app = new Framework7({
     // Enable Material theme for Android device only
     material: isAndroid ? true : false,
-    // Enable Template7 pages
-    template7Pages: true
+    template7Pages: true,
+    init: false,
+    modalTitle: 'TempStars'
 });
 
-// Init View
-var mainView = myApp.addView('.view-main', {
-    // Don't worry about that Material doesn't support it
-    // F7 will just ignore it for Material theme
-    dynamicNavbar: true
+// Initialize Views
+var mainView = app.addView('.view-main', {
+    dynamicNavbar: true,
+    domCache: false
+});
+$$('.view-main').hide();
+
+var landingView = app.addView('.view-landing', {
+    dynamicNavbar: false
 });
 
-$$(document).on( 'ajaxStart', function(e) {
-    console.log( 'ajax start' );
-});
-$$(document).on( 'ajaxError', function(e) {
-    console.dir( 'err' + JSON.stringify( e ) );
-});
-$$(document).on( 'ajaxSuccess', function(e) {
-    console.log( 'ajax success' );
-});
-$$(document).on( 'ajaxComplete', function(e) {
-    console.log( 'ajax complete' );
-});
+// Initialize App
+app.init();
 
-$$('#but').on( 'click', function(e) {
-    // For now just put get dentists here
-    console.log( 'click');
-    var dentistTemplate = $$('#DentistTemplate').html();
-    var compiledDentistTemplate = Template7.compile(dentistTemplate);
-
-    // works for android emulator $$.getJSON('http://10.0.2.2:3000/api/Dentists', function( serverData ) {
-    // works for everythig locally $$.getJSON('http://10.0.1.45:3000/api/Dentists', function( serverData ) {
-    //$$.getJSON('https://riff:raff@api.tempstars.net/api/Dentists', function( serverData ) {
-    $$.ajax({
-        cache: false,
-        contentType: 'application/json',
-        dataType: 'json',
-        username: 'riff',
-        password: 'raff',
-        timeout: 10 * 1000,
-        success: updateDentists,
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent('riff' + ':' + 'raff'))));
-        },
-        url: 'https://api.tempstars.net/v2/Dentists'
-        //url: 'http://10.0.1.45:3000/api/Dentists'
-    });
-
-function updateDentists( serverData ) {
-        console.log( 'got data');
-        //var dentists = { dentists: [ { practiceName: 'Fred'}, {practiceName:'Barney'} ] };
-        var dentistData = {};
-        dentistData.dentists = serverData;
-        var html = compiledDentistTemplate( dentistData );
-        $$('#dentist-list')[0].insertAdjacentHTML( 'afterbegin', html  );
+// If the user is already logged in, go directly to the main view
+if ( userLoggedIn ) {
+    $$('.view-landing').hide();
+    $$('.view-main').show();
+    if ( isDentist ) {
+        mainView.router.loadPage( { url: 'dentist.html', animatePages: false } );
+    }
+    else {
+        mainView.router.loadPage( { url: 'hygienist.html', animatePages: false } );
+    }
+    setupMenu();
 }
 
+
+app.onPageInit( 'login', function(page) {
+    $('#login-button').on( 'click', function(e) {
+        userLoggedIn = true;
+        isDentist = false;
+        $$('.view-landing').hide();
+        $$('.view-main').show();
+        mainView.router.loadPage( { url: 'hygienist.html', animatePages: false } );
+        mainView.router.reloadPage( 'hygienist.html' );
+        setupMenu();
+
+        $('#logout-link').on( 'click', function(e) {
+            app.confirm( 'Are you sure you want to log out?', logout );
+        });
+
+    });
 });
+
+app.onPageInit( 'signup', function(page) {
+    $('#signup-button').on( 'click', function(e) {
+        userLoggedIn = true;
+        isDentist = true;
+        mainView.router.loadPage( { url: 'dentist.html', animatePages: false } );
+        $$('.view-landing').hide();
+        $$('.view-main').show();
+        setupMenu();
+    });
+});
+
+app.onPageInit( 'forgot-password', function( page ) {
+    $('#forgot-password-button').on( 'click', function(e) {
+        $$('#forgot-password-status').show();
+    })
+});
+
+app.onPageInit( 'landing', function( page ) {
+    landingView.hideNavbar();
+});
+
+// app.onPageBeforeInit( 'login', function( page ) {
+//     landingView.showNavbar();
+// });
+//
+// app.onPageBeforeInit( 'signup', function( page ) {
+//     landingView.showNavbar();
+// });
+
+app.onPageBeforeInit( 'hygienist', function( page ) {
+    //mainView.showNavbar();
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August' , 'September' , 'October', 'November', 'December'];
+
+    var calendarInline = app.calendar({
+        container: '#calendar-inline-container',
+        value: [new Date()],
+        weekHeader: true,
+        firstDay: 0,
+        toolbarTemplate:
+            '<div class="toolbar calendar-custom-toolbar">' +
+                '<div class="toolbar-inner">' +
+                    '<div class="left">' +
+                        '<a href="#" class="link icon-only"><i class="icon icon-back"></i></a>' +
+                    '</div>' +
+                    '<div class="center"></div>' +
+                    '<div class="right">' +
+                        '<a href="#" class="link icon-only"><i class="icon icon-forward"></i></a>' +
+                    '</div>' +
+                '</div>' +
+            '</div>',
+        onOpen: function (p) {
+            $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
+            $$('.calendar-custom-toolbar .left .link').on('click', function () {
+                calendarInline.prevMonth();
+            });
+            $$('.calendar-custom-toolbar .right .link').on('click', function () {
+                calendarInline.nextMonth();
+            });
+        },
+        onMonthYearChangeStart: function (p) {
+            $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
+        },
+        events: [
+            new Date(2016, 7, 10),
+            new Date(2016, 7, 23)
+        ],
+
+        rangesClasses: [
+            {
+                cssClass: 'calendar-openings',
+                range: [
+                    new Date(2016, 7, 26),
+                    new Date(2016, 7, 29)
+                ]
+            },
+            {
+                cssClass: 'calendar-offers',
+                range: [
+                    new Date(2016, 7, 23),
+                    new Date(2016, 7, 24)
+                ]
+            },
+            {
+                cssClass: 'calendar-booked',
+                range: [
+                    new Date(2016, 7, 16),
+                    new Date(2016, 7, 17),
+                    new Date(2016, 7, 19)
+                ]
+            },
+            {
+                cssClass: 'calendar-worked',
+                range: [
+                    new Date(2016, 7, 2),
+                    new Date(2016, 7, 10)
+                ]
+            }
+        ]
+    });
+
+});
+
+$('#avail-posting-date-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$('#avail-work-date-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$('#avail-distance-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$$('.avail-book-button').on( 'click', function(e) {
+    app.confirm('Are you sure?', 'Book Job', function() {
+        app.alert( 'You\'re booked!' );
+    });
+});
+
+$('#offered-posting-date-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$('#offered-work-date-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$('#offered-distance-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+app.onPageBeforeInit( 'jobs', function( page ) {
+
+    $('#jobs-office-sorting-button').on( 'click', function(e) {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    $('#jobs-date-sorting-button').on( 'click', function(e) {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+});
+
+app.onPageBeforeInit( 'invoices', function( page ) {
+
+    $('#invoices-office-sorting-button').on( 'click', function(e) {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    $('#invoices-date-sorting-button').on( 'click', function(e) {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    $$('#invoices-create-manual-button').on( 'click', function(e) {
+        app.modal( { title: 'Create Manual Invoice',
+            text: 'Apple Valley Dental<br>Aug 16, 2016<br>8am - 6pm',
+            afterText: '<br>Offered hours:<br>from <input type="text" value="9:00"><br>to&nbsp;&nbsp;&nbsp; <input type="text" value="3:00">',
+            buttons: [
+                { text: 'Cancel'},
+                { text: 'Send Invoice', bold: true, onClick: sendInvoice }
+            ]
+        });
+    });
+});
+
+app.onPageBeforeInit( 'settings', function( page ) {
+    $$('.settings-remove-blocked-button').on( 'click', function(e) {
+        app.confirm('Are you sure?', 'Remove Blocked Office', function() {
+            app.alert( 'All set!' );
+        });
+    });
+
+    $$('.settings-remove-fav-button').on( 'click', function(e) {
+        app.confirm('Are you sure?', 'Remove Favourite Office', function() {
+            app.alert( 'All set!' );
+        });
+    });
+});
+
+$$('.offered-cancel-button').on( 'click', function(e) {
+    app.confirm('Are you sure?', 'Cancel Partial Offer', function() {
+        app.alert( 'You\'re cancelled!' );
+    });
+});
+
+$$('.booked-cancel-button').on( 'click', function(e) {
+    app.confirm('Are you sure?', 'Cancel Booked Job', function() {
+        app.alert( 'You\'re cancelled!' );
+    });
+});
+
+$$('.avail-partial-button').on( 'click', function(e) {
+    app.modal( { title: 'Make Partial Offer',
+        text: 'Apple Valley Dental<br>Aug 16, 2016<br>8am - 6pm',
+        afterText: '<br>Offered hours:<br>from <input type="text"><br>to <input type="text">',
+        buttons: [
+            { text: 'Cancel'},
+            { text: 'Make Offer', bold: true, onClick: makePartialOffer }
+        ]
+    });
+});
+
+function makePartialOffer() {
+    app.alert( 'You\'ve made a partial offer!' );
+}
+
+$$('.offered-modify-button').on( 'click', function(e) {
+    app.modal( { title: 'Modify Partial Offer',
+        text: 'Apple Valley Dental<br>Aug 16, 2016<br>8am - 6pm',
+        afterText: '<br>Offered hours:<br>from <input type="text" value="9:00"><br>to&nbsp;&nbsp;&nbsp; <input type="text" value="3:00">',
+        buttons: [
+            { text: 'Cancel'},
+            { text: 'Modify Offer', bold: true, onClick: makePartialOffer }
+        ]
+    });
+});
+
+$('#booked-posting-date-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$('#booked-work-date-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$('#booked-distance-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$('#completed-posting-date-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$('#completed-work-date-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$('#completed-distance-button').on( 'click', function(e) {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+$$('.completed-invoice-button').on( 'click', function(e) {
+    app.modal( { title: 'Create Invoice',
+        text: 'Apple Valley Dental<br>Aug 16, 2016<br>8am - 6pm',
+        afterText: '<br>Offered hours:<br>from <input type="text" value="9:00"><br>to&nbsp;&nbsp;&nbsp; <input type="text" value="3:00">',
+        buttons: [
+            { text: 'Cancel'},
+            { text: 'Send Invoice', bold: true, onClick: sendInvoice }
+        ]
+    });
+});
+
+function sendInvoice() {
+    app.alert( 'Invoice sent!' );
+}
+
+$$('.completed-survey-button').on( 'click', function(e) {
+    app.modal( { title: 'Take Survey',
+        text: 'Apple Valley Dental<br>Aug 16, 2016<br>8am - 6pm',
+        afterText: '<br>Offered hours:<br>from <input type="text" value="9:00"><br>to&nbsp;&nbsp;&nbsp; <input type="text" value="3:00">',
+        buttons: [
+            { text: 'Cancel'},
+            { text: 'Submit', bold: true, onClick: submitSurvey }
+        ]
+    });
+});
+
+function submitSurvey() {
+    app.alert( 'Survey submitted!' );
+}
+
+
+app.onPageBeforeInit( 'dentist', function( page ) {
+    mainView.showNavbar();
+});
+
+function setupMenu() {
+    var menuContent;
+    if ( isDentist ) {
+        menuContent = $('#dentist-menu').html();
+    }
+    else {
+        menuContent = $('#hygienist-menu').html();
+    }
+   $('#panel-menu').html(menuContent);
+}
+
+function logout() {
+    // Remove from local storage
+    app.closePanel();
+    userLoggedIn = false;
+    isDentist = false;
+    $$('.view-main').hide();
+    $$('.view-landing').show();
+    landingView.router.loadPage( { url: 'login.html', animatePages: false } );
+}
+
+function setupListeners() {
+
+}
