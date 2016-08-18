de3b58d96f2a41cb146efe857a557342dfdefd89
Mike Betts <mike@freshsqueezedwebsites.com>
2016-08-17 22:26:20 -0400

    add dentist prototype

--- app/platforms/ios/www/app/app.js
+++ app/platforms/ios/www/app/app.js
@@ -68,11 +68,6 @@ app.onPageInit( 'login', function(page) {
         mainView.router.loadPage( { url: 'hygienist.html', animatePages: false } );
         mainView.router.reloadPage( 'hygienist.html' );
         setupMenu();
-
-        $('#logout-link').on( 'click', function(e) {
-            app.confirm( 'Are you sure you want to log out?', logout );
-        });
-
     });
 });
 
@@ -177,6 +172,83 @@ app.onPageBeforeInit( 'hygienist', function( page ) {
     });
 
 });
+
+
+app.onPageBeforeInit( 'dentist', function( page ) {
+    //mainView.showNavbar();
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August' , 'September' , 'October', 'November', 'December'];
+
+    var calendarInline = app.calendar({
+        container: '#calendar-inline-container',
+        value: [new Date()],
+        weekHeader: true,
+        firstDay: 0,
+        toolbarTemplate:
+            '<div class="toolbar calendar-custom-toolbar">' +
+                '<div class="toolbar-inner">' +
+                    '<div class="left">' +
+                        '<a href="#" class="link icon-only"><i class="icon icon-back"></i></a>' +
+                    '</div>' +
+                    '<div class="center"></div>' +
+                    '<div class="right">' +
+                        '<a href="#" class="link icon-only"><i class="icon icon-forward"></i></a>' +
+                    '</div>' +
+                '</div>' +
+            '</div>',
+        onOpen: function (p) {
+            $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
+            $$('.calendar-custom-toolbar .left .link').on('click', function () {
+                calendarInline.prevMonth();
+            });
+            $$('.calendar-custom-toolbar .right .link').on('click', function () {
+                calendarInline.nextMonth();
+            });
+        },
+        onMonthYearChangeStart: function (p) {
+            $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
+        },
+        events: [
+            new Date(2016, 7, 10),
+            new Date(2016, 7, 23)
+        ],
+
+        rangesClasses: [
+            {
+                cssClass: 'calendar-openings',
+                range: [
+                    new Date(2016, 7, 26),
+                    new Date(2016, 7, 29)
+                ]
+            },
+            {
+                cssClass: 'calendar-offers',
+                range: [
+                    new Date(2016, 7, 23),
+                    new Date(2016, 7, 24)
+                ]
+            },
+            {
+                cssClass: 'calendar-booked',
+                range: [
+                    new Date(2016, 7, 16),
+                    new Date(2016, 7, 17),
+                    new Date(2016, 7, 19)
+                ]
+            },
+            {
+                cssClass: 'calendar-worked',
+                range: [
+                    new Date(2016, 7, 2),
+                    new Date(2016, 7, 10)
+                ]
+            }
+        ]
+    });
+
+});
+
+
+
 
 $('#avail-posting-date-button').on( 'click', function(e) {
     $(this).addClass('active').siblings().removeClass('active');
@@ -362,6 +434,11 @@ function setupMenu() {
         menuContent = $('#hygienist-menu').html();
     }
    $('#panel-menu').html(menuContent);
+
+   $('.logout-link').on( 'click', function(e) {
+       app.confirm( 'Are you sure you want to log out?', logout );
+   });
+
 }
 
 function logout() {
--- app/www/app/app.js
+++ app/www/app/app.js
@@ -65,14 +65,9 @@ app.onPageInit( 'login', function(page) {
         isDentist = false;
         $$('.view-landing').hide();
         $$('.view-main').show();
-        mainView.router.loadPage( { url: 'hygienist.html', animatePages: false } );
+        mainView.router.loadPage( { url: 'hygienist/hygienist.html', animatePages: false } );
         mainView.router.reloadPage( 'hygienist.html' );
         setupMenu();
-
-        $('#logout-link').on( 'click', function(e) {
-            app.confirm( 'Are you sure you want to log out?', logout );
-        });
-
     });
 });
 
@@ -80,7 +75,7 @@ app.onPageInit( 'signup', function(page) {
     $('#signup-button').on( 'click', function(e) {
         userLoggedIn = true;
         isDentist = true;
-        mainView.router.loadPage( { url: 'dentist.html', animatePages: false } );
+        mainView.router.loadPage( { url: 'dentist/dentist.html', animatePages: false } );
         $$('.view-landing').hide();
         $$('.view-main').show();
         setupMenu();
@@ -177,6 +172,83 @@ app.onPageBeforeInit( 'hygienist', function( page ) {
     });
 
 });
+
+
+app.onPageBeforeInit( 'dentist', function( page ) {
+    //mainView.showNavbar();
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August' , 'September' , 'October', 'November', 'December'];
+
+    var calendarInline = app.calendar({
+        container: '#calendar-inline-container',
+        value: [new Date()],
+        weekHeader: true,
+        firstDay: 0,
+        toolbarTemplate:
+            '<div class="toolbar calendar-custom-toolbar">' +
+                '<div class="toolbar-inner">' +
+                    '<div class="left">' +
+                        '<a href="#" class="link icon-only"><i class="icon icon-back"></i></a>' +
+                    '</div>' +
+                    '<div class="center"></div>' +
+                    '<div class="right">' +
+                        '<a href="#" class="link icon-only"><i class="icon icon-forward"></i></a>' +
+                    '</div>' +
+                '</div>' +
+            '</div>',
+        onOpen: function (p) {
+            $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
+            $$('.calendar-custom-toolbar .left .link').on('click', function () {
+                calendarInline.prevMonth();
+            });
+            $$('.calendar-custom-toolbar .right .link').on('click', function () {
+                calendarInline.nextMonth();
+            });
+        },
+        onMonthYearChangeStart: function (p) {
+            $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
+        },
+        events: [
+            new Date(2016, 7, 10),
+            new Date(2016, 7, 23)
+        ],
+
+        rangesClasses: [
+            {
+                cssClass: 'calendar-openings',
+                range: [
+                    new Date(2016, 7, 26),
+                    new Date(2016, 7, 29)
+                ]
+            },
+            {
+                cssClass: 'calendar-offers',
+                range: [
+                    new Date(2016, 7, 23),
+                    new Date(2016, 7, 24)
+                ]
+            },
+            {
+                cssClass: 'calendar-booked',
+                range: [
+                    new Date(2016, 7, 16),
+                    new Date(2016, 7, 17),
+                    new Date(2016, 7, 19)
+                ]
+            },
+            {
+                cssClass: 'calendar-worked',
+                range: [
+                    new Date(2016, 7, 2),
+                    new Date(2016, 7, 10)
+                ]
+            }
+        ]
+    });
+
+});
+
+
+
 
 $('#avail-posting-date-button').on( 'click', function(e) {
     $(this).addClass('active').siblings().removeClass('active');
@@ -362,7 +434,75 @@ function setupMenu() {
         menuContent = $('#hygienist-menu').html();
     }
    $('#panel-menu').html(menuContent);
+
+   $('.logout-link').on( 'click', function(e) {
+       app.confirm( 'Are you sure you want to log out?', logout );
+   });
+
 }
+
+app.onPageBeforeInit( 'post-job', function( page ) {
+
+    var jobCalendar = app.calendar({
+        input: '#job-calendar-input',
+        multiple: false
+    });
+
+    var today = new Date();
+
+    var jobStartTime = app.picker({
+        input: '#job-start-time-input',
+        toolbar: false,
+        rotateEffect: true,
+        value: [ '08:00'],
+        cols: [
+            { values: (function() {
+                    var vals = [],
+                        timeStr;
+                    for ( var i = 0; i < 24; i++ ) {
+                        for ( var j = 0; j <  60; j = j + 15 ) {
+                            timeStr = ('00' + i).slice(-2);
+                            timeStr += ':' + ('00' + j).slice(-2);
+                            vals.push( timeStr );
+                        }
+                    }
+                    return vals;
+                })()
+        }]
+    });
+
+    var jobEndTime = app.picker({
+        input: '#job-end-time-input',
+        toolbar: false,
+        rotateEffect: true,
+        value: [ '05:00'],
+        cols: [
+            { values: (function() {
+                    var vals = [],
+                        timeStr;
+                    for ( var i = 0; i < 24; i++ ) {
+                        for ( var j = 0; j <  60; j = j + 15 ) {
+                            timeStr = ('00' + i).slice(-2);
+                            timeStr += ':' + ('00' + j).slice(-2);
+                            vals.push( timeStr );
+                        }
+                    }
+                    return vals;
+                })()
+        }]
+    });
+
+    $$('#job-post-button').on( 'click', function(e) {
+        app.confirm('Are you sure?', 'Post Job', function() {
+            app.alert( 'Job posted!', function() {
+                mainView.router.back( { pageName: 'dentist'});
+            });
+        });
+    });
+
+
+});
+
 
 function logout() {
     // Remove from local storage
@@ -371,7 +511,7 @@ function logout() {
     isDentist = false;
     $$('.view-main').hide();
     $$('.view-landing').show();
-    landingView.router.loadPage( { url: 'login.html', animatePages: false } );
+    landingView.router.loadPage( { url: 'landing/login.html', animatePages: false } );
 }
 
 function setupListeners() {
