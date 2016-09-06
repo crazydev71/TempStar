b29edaff9a1e81c6626c220b188c27409627f21d
Mike Betts <mike@freshsqueezedwebsites.com>
2016-09-05 23:17:31 -0400

    enable "me" in place of user id

--- api/boot/authentication.js
+++ api/boot/authentication.js
@@ -1,4 +1,12 @@
 module.exports = function enableAuthentication(server) {
-  // enable authentication
+  // Enable authentication
   server.enableAuth();
+
+  // Enable using "me" instead of user id
+  var loopback = require('loopback');
+  server.middleware('auth', loopback.token({
+    model: server.models.accessToken,
+    currentUserLiteral: 'me'
+  }));
+
 };
