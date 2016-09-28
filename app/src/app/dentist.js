18ea1e2028fcda0c7971dc0180c4e11073aef9fc
Mike Betts <mike@freshsqueezedwebsites.com>
2016-09-28 10:21:32 -0400

    update dentist api wrapper calls

--- app/src/app/dentist.js
+++ app/src/app/dentist.js
@@ -6,6 +6,40 @@ TempStars.Dentist = (function() {
         setupAccount: function setupAccount( data ) {
             var dentistId = data.user.dentistId;
             return TempStars.Api.setupDentistAccount( dentistId, data );
+        },
+
+        getAllJobs: function getAllJobs() {
+            return TempStars.Api.getJobs( TempStars.User.getCurrentUser().dentistId );
+        },
+
+        getJob: function getJob( jobId ) {
+            return TempStars.Api.getJob( TempStars.User.getCurrentUser().dentistId, jobId );
+        },
+
+        getPosted: function getPosted() {
+            var filter = '{"where":{"status":' + status.POSTED + '}';
+            return TempStars.Api.getJobs( TempStars.User.getCurrentUser().dentistId, filter );
+        },
+
+        getWithPartialOffers: function getWithPartialOffers() {
+            var filter = '{"where":{"status":' + status.PARTIAL + '}';
+            return TempStars.Api.getJobs( TempStars.User.getCurrentUser().dentistId, filter );
+        },
+
+        getConfirmed: function getConfirmed() {
+            var filter = '{"where":{"status":' + status.CONFIRMED + '}';
+            return TempStars.Api.getJobs( TempStars.User.getCurrentUser().dentistId, filter );
+        },
+
+        getCompleted: function getCompleted() {
+            var filter = '{"where":{"status":' + status.COMPLETED + '}';
+            return TempStars.Api.getJobs( TempStars.User.getCurrentUser().dentistId, filter );
+        },
+
+        getJobsByDate: function getJobByDate( date ) {
+            var filter = {where: {startDate: date }};
+            var filterString = JSON.stringify( filter );
+            return TempStars.Api.getJobs( TempStars.User.getCurrentUser().dentistId, filterString );
         }
     };
 })();
