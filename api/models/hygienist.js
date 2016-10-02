d470f89544bc8ecf7941c3a9f731d4c66f141746
Mike Betts <mike@freshsqueezedwebsites.com>
2016-10-02 16:31:03 -0400

    removed bogus duplicate function

--- api/models/hygienist.js
+++ api/models/hygienist.js
@@ -17,57 +17,59 @@ module.exports = function( Hygienist ) {
 
     Hygienist.disableRemoteMethod('createChangeStream', true);
 
-    Hygienist.remoteMethod( 'account', {
-        accepts: [
-            {arg: 'id', type: 'number', required: true},
-            {arg: 'data', type: 'object', http: { source: 'body' } } ],
-        returns: { arg: 'result', type: 'object' },
-        http: { verb: 'put', path: '/:id/account' }
-    });
-
-    Hygienist.account = function( id, data, callback ) {
-        var hygienist;
-        var geocode;
-
-        Hygienist.findById( id )
-        .then( function( h ) {
-            hygienist = h;
-            data.isComplete = 1;
-            return hygienist.updateAttributes( data );
-        })
-        .then( function() {
-            return new Promise( function( resolve, reject ) {
-                var address = hygienist.address + ', ' + hygienist.city + ', ' + hygienist.province + ' ' + hygienist.postalCode + ' Canada';
-                location.geocode( address )
-                .then( function( latlon ) {
-                    resolve( latlon );
-                })
-                .catch( function( err ) {
-                    resolve( { lat: 0, lon: 0} );
-                });
-            });
-        })
-        .then( function( gc ) {
-            geocode = gc;
-            var PostalCode = app.models.PostalCode;
-            var prefix = hygienist.postalCode.substr(0,2);
-            return PostalCode.findOne( {where: {prefix: prefix} } );
-        })
-        .then( function( postalCode ) {
-            return hygienist.updateAttributes( {
-                lat: geocode.lat,
-                lon: geocode.lng,
-                regionId: postalCode.regionId
-            });
-        })
-        .then( function() {
-            callback( null, {} );
-        })
-        .catch( function( err ) {
-            callback( err );
-        });
-    };
-
+    // Hygienist.remoteMethod( 'account', {
+    //     accepts: [
+    //         {arg: 'id', type: 'number', required: true},
+    //         {arg: 'data', type: 'object', http: { source: 'body' } } ],
+    //     returns: { arg: 'result', type: 'object' },
+    //     http: { verb: 'put', path: '/:id/account' }
+    // });
+    //
+    // Hygienist.account = function( id, data, callback ) {
+    //     var hygienist;
+    //     var geocode;
+    //
+    //     Hygienist.findById( id )
+    //     .then( function( h ) {
+    //         hygienist = h;
+    //         data.isComplete = 1;
+    //         return hygienist.updateAttributes( data );
+    //     })
+    //     .then( function(h) {
+    //         console.log( 'after update hygienist');
+    //         console.dir( h );
+    //         return new Promise( function( resolve, reject ) {
+    //             var address = hygienist.address + ', ' + hygienist.city + ', ' + hygienist.province + ' ' + hygienist.postalCode + ' Canada';
+    //             location.geocode( address )
+    //             .then( function( latlon ) {
+    //                 resolve( latlon );
+    //             })
+    //             .catch( function( err ) {
+    //                 resolve( { lat: 0, lon: 0} );
+    //             });
+    //         });
+    //     })
+    //     .then( function( gc ) {
+    //         geocode = gc;
+    //         var PostalCode = app.models.PostalCode;
+    //         var prefix = hygienist.postalCode.substr(0,2);
+    //         return PostalCode.findOne( {where: {prefix: prefix} } );
+    //     })
+    //     .then( function( postalCode ) {
+    //         return hygienist.updateAttributes( {
+    //             lat: geocode.lat,
+    //             lon: geocode.lng,
+    //             regionId: postalCode.regionId
+    //         });
+    //     })
+    //     .then( function() {
+    //         callback( null, {} );
+    //     })
+    //     .catch( function( err ) {
+    //         callback( err );
+    //     });
+    // };
+    //
 
     Hygienist.remoteMethod( 'updateAccount', {
         accepts: [
@@ -84,16 +86,7 @@ module.exports = function( Hygienist ) {
         Hygienist.findById( id )
         .then( function( h ) {
             hygienist = h;
-            return hygienist.updateAttributes({
-                firstName: data.firstName,
-                lastName: data.lastName,
-                phone: data.phone,
-                address: data.address,
-                city: data.city,
-                province: data.province,
-                postalCode: data.postalCode,
-                photoUrl: data.photoUrl
-            });
+            return hygienist.updateAttributes( data );
         })
         .then( function( h ) {
             hygienist = h;
